eac463e0f8d3ed08c4ed0775c4fb850d2e951147
bacfrb15 <bacfrb15@htlkaindorf.at>
2018-10-03 12:55:17 +0200

    Basisimplementierung des TableModels

--- Bsp_201_JTable_VelMessung/src/ex005/VelocityTableModel.java
+++ Bsp_201_JTable_VelMessung/src/ex005/VelocityTableModel.java
@@ -5,10 +5,50 @@
  */
 package ex005;
 
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
 /**
  *
  * @author franz
  */
-public class VelocityTableModel {
+public class VelocityTableModel extends AbstractTableModel{
+
+    private static String[] COLNAMES = {"Datum","Uhrzeit","Kennzeichen","Gemessen", "Erlaubt", "Übertretung"};
+    private ArrayList<Measurement> measures = new ArrayList<>();
+    private DateTimeFormatter dtfDatum = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private DateTimeFormatter dtfUhrzeit = DateTimeFormatter.ofPattern("hh:mm");
+    
+    @Override
+    public int getRowCount() {
+        return measures.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return COLNAMES.length;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return COLNAMES[column];
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Measurement m = measures.get(rowIndex);
+        switch(columnIndex)
+        {
+            case 0: return m.getLdt().format(dtfDatum);
+            case 1: return m.getLdt().format(dtfUhrzeit);
+            case 2: return m.getKennzeichen();
+            case 3: return m.getGemessen();
+            case 4: return m.getErlaubt();
+            case 5: return m.getGemessen()-m.getErlaubt();
+            
+            default: return "???";
+        }
+    }
     
 }
